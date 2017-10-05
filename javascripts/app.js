0e7aef75cae8936cefb0867b546780ec65ea7c7a
Dario Vigil <dariovigil@gmail.com>
2017-10-05 19:27:12 +0200

    Finishing rover logic

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,12 +1,15 @@
 // Rover Object Goes Here
 // ======================
 const rover = {
-  x: 0,
-  y: 0,
+  x: 5,
+  y: 5,
   facingDirection: 'N',
   travelLog: [],
+  printMovement(movement) {
+    console.log(`Rover moves ${movement}, it's position is (${this.x}, ${this.y})`);
+  },
   printTravelLog() {
-    console.log(`travelLog is updated: ${this.travelLog}`);
+    console.log(`TravelLog is updated: ${this.travelLog}`);
   },
   turn(turnDirection) {
     switch (turnDirection) {
@@ -35,22 +38,22 @@ const rover = {
       default:
         console.log('ERROR: The valid parameters are "left" or "right"');
     }
-    console.log(this.facingDirection);
+    console.log(`Mars rover has turned ${turnDirection}, now it is facing: ${this.facingDirection}`);
   },
   move(direction) {
     switch (direction) {
       case 'forward':
-        if (this.facingDirection === 'N') {
+        if (this.facingDirection === 'N' && this.y > 0) {
           this.y -= 1;
-          // Check if out of bounds
-          this.travelLog.push([`(${this.x}, ${this.y})`]);
-
-          console.log(`rover moves forward, it's position is (${this.x}, ${this.y})`);
         }
-        if (this.facingDirection === 'S') {
+        if (this.facingDirection === 'S' && this.y < 9) {
           this.y += 1;
-          // Check if out of bounds
-          console.log(this.y);
+        }
+        if (this.facingDirection === 'E' && this.x < 9) {
+          this.x += 1;
+        }
+        if (this.facingDirection === 'W' && this.x > 0) {
+          this.x -= 1;
         }
         break;
       case 'backwards':
@@ -59,6 +62,9 @@ const rover = {
         break;
       default:
     }
+    this.travelLog.push([`(${this.x}, ${this.y})`]);
+    this.printMovement(direction);
+    this.printTravelLog();
   },
 };
 
@@ -76,12 +82,13 @@ function createMap(rows, cols, defaultValue) {
   return arr;
 }
 const marsMap = createMap(10, 10, '_');
-marsMap[9][0] = 'X';
+marsMap[rover.x][rover.y] = 'X';
 console.log(marsMap);
 
 function setRoverPosition(posX, posY) {
   marsMap[posX][posY] = 'X';
   console.log(`Mars rover is in coordinates: ${rover.x}, ${rover.y}`);
+  console.log(`Mars rover is facing: ${rover.facingDirection}`);
 }
 setRoverPosition(rover.x, rover.y);
 
@@ -90,24 +97,46 @@ setRoverPosition(rover.x, rover.y);
 const sendCommandsBtn = document.getElementById('btn-send-commands');
 const inputTextCommands = document.getElementById('input-text-commands');
 
+// function sendCommands(commands) {
+//   [...commands].forEach((char) => {
+//     if (char === 'f') {
+//       rover.move('forward');
+//     } else if (char === 'b') {
+//       rover.move('backwards');
+//     }
+//   });
+//   rover.printTravelLog();
+// }
+
 function sendCommands(commands) {
   [...commands].forEach((char) => {
-    if (char === 'f') {
-      rover.move('forward');
-    } else if (char === 'b') {
-      rover.move('backwards');
+    switch (char) {
+      case 'f':
+        rover.move('forward');
+        break;
+      case 'b':
+        rover.move('backwards');
+        break;
+      case 'r':
+        rover.turn('right');
+        break;
+      case 'l':
+        rover.turn('left');
+        break;
+      default:
+        console.log('invalid input');
     }
   });
-  rover.printTravelLog();
 }
 
 function validateCommands() {
   const commands = inputTextCommands.value;
   const re = new RegExp(/^['f', 'r', 'l', 'b']+$/);
   if (!commands) {
-    console.log('commands field is empty!');
+    console.log('Input field is empty!');
   } else if (commands.match(re)) {
     sendCommands(commands);
+    inputTextCommands.value = '';
   } else {
     console.log('invalid command!');
   }
